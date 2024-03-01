bf3b81cec97ab68aed93fd2288d0f91087276f9b
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2024-02-29 19:56:37 -0800

    Update tailwind.config.ts

--- rated-collections-app/tailwind.config.ts
+++ rated-collections-app/tailwind.config.ts
@@ -1,18 +1,38 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      primary: '#93D5AA',
+      secondary: '#B5CCBA',
+      'on-surface': '#DFE4DD',
+      'on-surface-variant': '#C0C9C0',
+      'surface-container': '#1C211D',
+      'surface-container-high': '#262B27',
+    },
+    fontSize: {
+      'body-lg': ['16px', '24px'],
+    },
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      rotate: {
+        '270': '270deg',
+      },
+      transitionDuration: {
+        '2000': '2000ms',
+      },
+      flex: {
+        '2': '2 2 0%',
+      }
     },
   },
   plugins: [],
