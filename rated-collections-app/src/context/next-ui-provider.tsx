5098f7d83a4413948cfe2f280e7c1d639e6ad719
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2024-03-03 05:24:48 -0800

    Remove NextUIProviderWrapper component

--- rated-collections-app/src/context/next-ui-provider.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import { NextUIProvider } from '@nextui-org/react';
-
-export function NextUIProviderWrapper({
-  children,
-}: Readonly<{ children: React.ReactNode }>) {
-  return <NextUIProvider>{children}</NextUIProvider>;
-}
