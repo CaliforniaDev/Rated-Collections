1b4f2e91a587705b23ef66a82c9282b19caf400b
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2024-03-04 04:49:26 -0800

    Update page component with new button and theme switcher

--- rated-collections-app/src/app/page.tsx
+++ rated-collections-app/src/app/page.tsx
@@ -1,10 +1,15 @@
 import Image from 'next/image';
-import { Button } from '@nextui-org/button';
+import { Button } from '@nextui-org/react';
+import { ThemeSwitcher } from '@/components/theme/ThemeSwitcher';
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <Button>Click Me!</Button>
+    <main className='flex min-h-screen gap-4 flex-col p-24'>
+      <Button color="primary" className="dark:primary" variant="solid" radius="md">Click Me!</Button>
+      <div className="size-40 text-primary-foreground bg-primary">
+        <p>Hello Next JS</p>
+      </div>
+      <ThemeSwitcher />
     </main>
   );
 }
