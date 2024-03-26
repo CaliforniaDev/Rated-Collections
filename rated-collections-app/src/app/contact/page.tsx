097eff8ef51c7c5ee112b8568d4666195374026f
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2024-03-26 05:31:10 -0700

    Add page files for all link in navbar for testing

---
+++ rated-collections-app/src/app/contact/page.tsx
@@ -0,0 +1,8 @@
+export default function ContactPage() {
+  return (
+  <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+      <h1 className='text-4xl'>Contact Us</h1>
+      <p className='mt-4 text-lg'>Email: r8ted@r8tedcollection.com </p>
+    </main>
+  );
+}
--- rated-collections-app/src/app/page.tsx
+++ rated-collections-app/src/app/page.tsx
@@ -3,7 +3,13 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-
+      <h1 className='text-4xl'>Welcome to Rated Collections</h1>
+      <Image
+        src='/images/navigation/logo.svg'
+        alt='R8TED Collections'
+        width={400}
+        height={400}
+      />
     </main>
   );
 }
