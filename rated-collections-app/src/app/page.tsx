6994d9a0027a11d1ab955eab1ed5cb02990e58bc
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2024-03-27 06:04:38 -0700

    Add top padding to main html element to keep navbar from overlapping content

--- rated-collections-app/src/app/page.tsx
+++ rated-collections-app/src/app/page.tsx
@@ -1,15 +1,21 @@
 import Image from 'next/image';
+import HeroSection from '@/components/sections/HeroSection';
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <h1 className='text-4xl'>Welcome to Rated Collections</h1>
-      <Image
-        src='/images/navigation/logo.svg'
-        alt='R8TED Collections'
-        width={400}
-        height={400}
-      />
+    <main className='flex min-h-screen flex-col items-center justify-between py-20'>
+      <HeroSection
+        backgroundImage='/images/hero/hollywood-sunset.webp'
+        overlayColor='#181D19'
+      >
+        <h1 className='text-4xl'>Welcome to Rated Collections</h1>
+        <Image
+          src='/images/navigation/logo.svg'
+          alt='R8TED Collections'
+          width={400}
+          height={400}
+        />
+      </HeroSection>
     </main>
   );
 }
