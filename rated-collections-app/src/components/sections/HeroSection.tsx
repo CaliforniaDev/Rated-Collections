2cf4f433f0c6e99fa681c695e4bb7bacabc2f125
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2024-04-01 03:50:59 -0700

    Refactor HeroSection component to use StaticImageData for backgroundImage
    -Next JS made changes to Image components since version 13. Refactoring reflect those changes

--- rated-collections-app/src/components/sections/HeroSection.tsx
+++ rated-collections-app/src/components/sections/HeroSection.tsx
@@ -1,26 +1,25 @@
-import Image from 'next/image';
+import Image, { StaticImageData } from 'next/image';
 type HeroProps = {
-  backgroundImage: string;
+  backgroundImage: StaticImageData | string;
   overlayColor: string;
   children: React.ReactNode;
 };
 
 function HeroSection({ backgroundImage, overlayColor, children }: HeroProps) {
   return (
-    <div className='fullscreen-minus-nav relative w-full'>
+    <div className='fullscreen-minus-nav relative flex w-full items-center justify-center'>
       <div className='-z-10'>
         <Image
           priority
           src={backgroundImage}
-          layout='fill'
-          objectFit='cover'
-          objectPosition='center'
+          fill
+          className="object-cover object-center"
           alt='Hero Image'
         />
       </div>
       <div
-        className='absolute inset-0'
-        style={{ backgroundColor: overlayColor}}
+        className='absolute inset-0 flex p-12'
+        style={{ backgroundColor: overlayColor }}
       >
         {children}
       </div>
