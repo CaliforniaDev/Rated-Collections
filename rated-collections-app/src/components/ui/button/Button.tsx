d1d5c06b9f64b1b2c99f3b6f14906a158ef3258e
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2024-04-03 05:42:11 -0700

    Add route prop to Button component and use it to handle onClick behaviors

--- rated-collections-app/src/components/ui/button/Button.tsx
+++ rated-collections-app/src/components/ui/button/Button.tsx
@@ -1,10 +1,22 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+
 type ButtonProps = {
   children: string;
+  route?: string;
   className?: string;
   variant?: 'filled' | 'outlined';
 };
 
-function Button({ children, className = '', variant = 'filled' }: ButtonProps) {
+function Button({
+  children,
+  route = '',
+  className = '',
+  variant = 'filled',
+}: ButtonProps) {
+  const router = useRouter();
+
   // Common button styles
   const baseClasses = 'btn-base-styles';
 
@@ -13,11 +25,19 @@ function Button({ children, className = '', variant = 'filled' }: ButtonProps) {
     variant === 'filled'
       ? 'bg-primary text-on-primary dark:bg-primary-dark dark:text-on-primary-dark'
       : 'border border-primary text-primary dark:border-primary-dark dark:text-primary-dark';
-  
+
   // Combine all classes
   const buttonClasses = `${baseClasses} ${variantClasses} ${className}`;
 
-  return <button className={buttonClasses}>{children}</button>;
+  return (
+    <button
+      type='button'
+      onClick={() => router.push(route)}
+      className={buttonClasses}
+    >
+      {children}
+    </button>
+  );
 }
 
 export default Button;
