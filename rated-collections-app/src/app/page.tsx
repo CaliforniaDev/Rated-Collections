2c9082ec93596e8002b97888f03fd0eb5ada73cf
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2024-04-08 04:34:56 -0700

    feat: Enhance HeroSection responsiveness across devices

--- rated-collections-app/src/app/page.tsx
+++ rated-collections-app/src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <main className='relative flex min-h-screen flex-col items-center justify-center overflow-hidden pt-20'>
       <HeroSection backgroundImage={heroBg} overlayColor='rgba(18,29,25, 0.9)'>
-        <div className='bg-primary relative flex h-full w-full overflow-hidden rounded-xl'>
+        <div className='bg-primary relative flex h-2/3 w-full overflow-hidden rounded-xl lg:h-full'>
           <Image
             priority
             src={heroImage}
@@ -19,7 +19,9 @@ export default function Home() {
             sizes='(min-width: 1200px) 1300px, (min-width: 768px) 650px, 100vw'
             alt="Woman wearing a black shirt with logo 'Rated Collection' on it."
           />
-          <header className='text-display-lg absolute ml-28 mt-40 w-2/5 uppercase'>
+
+          {/* Shows only on tablet or desktop devices */}
+          <header className='text-display-lg absolute ml-10 mt-20 hidden w-3/5 flex-col uppercase sm:flex md:w-2/5 lg:ml-28 lg:mt-40'>
             <h1 className='text-display-sm md:text-display-md lg:text-display-lg'>
               <span className='text-primary dark:text-primary-dark'>
                 R8ted{' '}
@@ -33,6 +35,15 @@ export default function Home() {
               <Button variant='outlined'>Subscribe</Button>
             </div>
           </header>
+          
+        </div>
+
+        {/* Shows only on mobile devices */}
+        <div className='mt-6 flex flex-col gap-6 sm:hidden'>
+          <Button route='/shop' variant='filled'>
+            Shop Now
+          </Button>
+          <Button variant='outlined'>Subscribe</Button>
         </div>
       </HeroSection>
     </main>
