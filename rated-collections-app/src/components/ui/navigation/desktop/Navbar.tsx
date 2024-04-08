e828b543d652d3e5a2e80fd5ca32126c1576866c
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2024-04-08 02:04:10 -0700

    feat: integrate mobile navigation into Navbar
    Enhance the Navbar component with mobile-friendly navigation capabilities.
    This update introduces a stateful BurgerButton to toggle the visibility
    of a MobileMenu, providing a responsive and intuitive user interface for
    smaller screens. Adjustments include hiding NavLinks on mobile devices and
    showcasing them only on larger screens (lg:flex), ensuring a clutter-free
    mobile experience. The addition of React state management for the menu's
    open/close state highlights a dynamic and interactive navigation approach.

--- rated-collections-app/src/components/ui/navigation/desktop/Navbar.tsx
+++ rated-collections-app/src/components/ui/navigation/desktop/Navbar.tsx
@@ -1,42 +1,55 @@
+'use client';
+
 import './styles.css';
-import Image from 'next/image';
+import { useState } from 'react';
 import Link from 'next/link';
-import logo from '@/assets/svg/navigation/logo.svg';
-import SearchBar from '@/components/ui/search/searchbar';
-import BagIcon from '@/assets/svg/navigation/BagIcon';
-import { ThemeSwitcher } from '@/theme/ThemeSwitcher';
+import Image from 'next/image';
+
 import NavLinks from '@/components/ui/navigation/nav-links/NavLinks';
+import logo from '@/assets/svg/navigation/logo.svg';
+import BagIcon from '@/assets/svg/navigation/BagIcon';
+
+import SearchBar from '@/components/ui/search/searchbar';
+import { ThemeSwitcher } from '@/theme/ThemeSwitcher';
+
+// Mobile Navigation
+import BurgerButton from '@/components/ui/navigation/mobile/BurgerButton';
+import MobileMenu from '@/components/ui/navigation/mobile/MobileMenu';
 
 export default function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
     <header className='fixed z-50 w-screen shadow'>
-      <nav className='bg-surface-container dark:bg-surface-container-dark h-20 px-12 py-3'>
+      <nav className='bg-surface-container dark:bg-surface-container-dark relative h-20 px-12 py-3'>
         {/* Content parent wrapper */}
         <div className='flex h-full justify-between gap-12'>
           {/* Logo */}
           <div className=' flex  min-w-20 basis-1/5'>
             <Link href='/'>
-              <Image
-                src={logo}
-                alt='R8TED Collections'
-                width={74}
-              />
+              <Image src={logo} alt='R8TED Collections' width={74} />
             </Link>
           </div>
 
           {/* Nav Links */}
-          <div className='flex basis-3/5 items-center justify-center'>
+          <div className='hidden basis-3/5 items-center justify-center lg:flex'>
             <NavLinks />
           </div>
 
-          {/* Search and Bag Icons, Theme Switcher */}
+          {/* Nav Icon Buttons */}
           <div className='flex grow basis-1/5 items-center justify-end gap-10'>
             <BagIcon className='fill-on-surface dark:fill-on-surface-dark' />
             <div className='w-6'>
               <ThemeSwitcher />
             </div>
             <SearchBar />
+            <MobileMenu isOpen={isMenuOpen} />
+            <BurgerButton
+              isOpen={isMenuOpen}
+              onClick={() => setIsMenuOpen(!isMenuOpen)}
+            />
           </div>
+
+          {/* Mobile Navigation */}
         </div>
       </nav>
     </header>
