56a393bd9dc3a1273c53625c5b9b38437c88dfce
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2024-04-15 01:01:04 -0700

    Refactor Navbar component and update styles

--- rated-collections-app/src/components/ui/navigation/desktop/Navbar.tsx
+++ rated-collections-app/src/components/ui/navigation/desktop/Navbar.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 'use client';
 
 import './styles.css';
@@ -54,44 +53,5 @@ export default function Navbar() {
         </div>
       </nav>
     </header>
-=======
-import './styles.css';
-import Image from 'next/image';
-import Link from 'next/link';
-import SearchBar from '@/components/ui/search/searchbar';
-import BagIcon from '@/assets/svg/navigation/BagIcon';
-import { ThemeSwitcher } from '@/theme/ThemeSwitcher';
-import NavLinks from '@/components/ui/navigation/nav-links/NavLinks';
-
-export default function Navbar() {
-  return (
-    <nav className='bg-surface-container dark:bg-surface-container-dark fixed h-20 w-screen px-6 py-3 shadow'>
-      <div className='flex h-full justify-between gap-12'>
-        <div className=' flex  min-w-20 basis-1/5 justify-center'>
-          <Link href='/'>
-            <Image
-              src='/images/navigation/logo.svg'
-              alt='R8TED Collections'
-              width={0}
-              height={0}
-              style={{ width: '74px', height: 'auto' }}
-            />
-          </Link>
-        </div>
-
-        <div className='flex basis-3/5 items-center justify-center'>
-          <NavLinks />
-        </div>
-
-        <div className='flex grow basis-1/5 items-center justify-end gap-10'>
-          <BagIcon className='fill-on-surface dark:fill-on-surface-dark' />
-          <div className='w-6'>
-            <ThemeSwitcher />
-          </div>
-          <SearchBar />
-        </div>
-      </div>
-    </nav>
->>>>>>> origin/main
   );
 }
