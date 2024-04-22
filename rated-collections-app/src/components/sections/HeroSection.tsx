c56fd5313cd5376ad7b7fa616139a8b134492a81
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2024-04-22 04:37:42 -0700

    Refactor HeroSection component to use grid layout instead of absolute positioning and removing background image

--- rated-collections-app/src/components/sections/HeroSection.tsx
+++ rated-collections-app/src/components/sections/HeroSection.tsx
@@ -1,28 +1,12 @@
 import Image, { StaticImageData } from 'next/image';
 type HeroProps = {
-  backgroundImage: StaticImageData | string;
-  overlayColor: string;
   children: React.ReactNode;
 };
 
-function HeroSection({ backgroundImage, overlayColor, children }: HeroProps) {
+function HeroSection({ children }: HeroProps) {
   return (
-    <div className='fullscreen-minus-nav relative flex w-full items-center justify-center'>
-      <div className='-z-10'>
-        <Image
-          priority
-          src={backgroundImage}
-          fill
-          className='object-cover object-center'
-          alt='Background image of hollywood sign'
-        />
-      </div>
-      <div
-        className='absolute inset-0 flex flex-col p-6 md:p-12'
-        style={{ backgroundColor: overlayColor }}
-      >
-        {children}
-      </div>
+    <div className='fullscreen-minus-nav relative grid grid-cols-3 gap-2 border p-6 md:p-12 '>
+      {children}
     </div>
   );
 }
