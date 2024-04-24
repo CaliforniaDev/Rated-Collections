12f7122d4b6c397870399030fcf8d0948fb7e9eb
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2024-04-24 06:20:41 -0700

    fix: Update header styling

--- rated-collections-app/src/app/page.tsx
+++ rated-collections-app/src/app/page.tsx
@@ -3,11 +3,12 @@ import HeroSection from '@/components/sections/HeroSection';
 import heroImage from '@/../public/images/hero/bi-racial-couple_purple-white.webp';
 import smallHeroImage from '@/../public/images/hero/female-prince-hero.webp';
 import Button from '@/components/ui/buttons/Button';
+import Overlay from '@/components/ui/overlay/Overlay';
 export default function Home() {
   return (
     <main className='pt-nav-height relative flex min-h-screen flex-col overflow-hidden'>
       <HeroSection>
-        <div className='bg-primary relative col-span-2 flex h-2/3 overflow-hidden rounded-xl lg:h-full'>
+        <div className='hidden bg-primary relative col-span-2 md:flex items-end overflow-hidden rounded-xl p-10 h-full'>
           <Image
             priority
             src={heroImage}
@@ -18,14 +19,14 @@ export default function Home() {
             sizes='(min-width: 1200px) 1300px, (min-width: 768px) 650px, 100vw'
             alt="Woman wearing a black shirt with logo 'Rated Collection' on it."
           />
-
+          <Overlay />
           {/* Shows only on tablet or desktop devices */}
-          <header className='text-display-lg absolute ml-10 mt-20 hidden w-3/5 flex-col uppercase sm:flex md:w-2/5 lg:ml-28 lg:mt-40'>
-            <h1 className='text-display-sm md:text-display-md lg:text-display-lg'>
+          <header className='text-display-lg relative w-4/6 '>
+            <h1 className='text-display-sm md:text-display-md lg:text-display-md uppercase'>
               <span className='text-primary dark:text-primary-dark'>
                 R8ted{' '}
               </span>
-              or not here we come!
+              or Not Here We Come!
             </h1>
             <div className='mt-7 flex gap-4'>
               <Button route='/shop' variant='filled'>
@@ -45,15 +46,16 @@ export default function Home() {
             className='object-cover'
             alt='woman wearing white t shirt'
           />
+          <Overlay />
         </div>
 
-        {/* Shows only on mobile devices */}
+        {/* Shows only on mobile devices
         <div className='mt-6 flex flex-col gap-6 sm:hidden'>
           <Button route='/shop' variant='filled'>
             Shop Now
           </Button>
           <Button variant='outlined'>Subscribe</Button>
-        </div>
+        </div> */}
       </HeroSection>
     </main>
   );
