6e52152a796ed8f8367a63062c4425c26f058992
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2024-04-24 05:42:33 -0700

    Update BgImage component to use new background styles and add alt text for accessibility

--- rated-collections-app/src/components/ui/background/BgImage.tsx
+++ rated-collections-app/src/components/ui/background/BgImage.tsx
@@ -3,16 +3,14 @@ import bgImage from '@/../public/images/background/hollywood-sunset.webp';
 
 export default function BgImage() {
   return (
-    <div className='fixed'>
+    <div className='fixed h-screen w-screen'>
       <Image
         src={bgImage}
+        alt='image of hollywood sign and palm trees.'
+        fill
+        className='-z-10 object-cover'
         placeholder='blur'
         sizes='100vw'
-        style={{
-          objectFit: 'cover',
-          zIndex: '-1',
-        }}
-        alt='background image of hollywood sign.'
       />
       <div className=' bg-surface-container dark:bg-surface-container-dark absolute inset-0 opacity-70 dark:opacity-80 '></div>
     </div>
