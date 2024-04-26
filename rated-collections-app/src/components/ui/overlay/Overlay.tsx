b1d72627b2e924af18a9457025eff8fda7960fc9
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2024-04-25 21:50:55 -0700

    Add Overlay component for UI overlay in rated-collections-app

---
+++ rated-collections-app/src/components/ui/overlay/Overlay.tsx
@@ -0,0 +1,12 @@
+export default function Overlay() {
+  return (
+    <div
+      style={{
+        opacity: 0.8,
+        background:
+          'linear-gradient(180deg, rgba(66, 80, 69, 0.00) 0%, #181D19 75.5%)',
+      }}
+      className='absolute inset-0'
+    ></div>
+  );
+}
